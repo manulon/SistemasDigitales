e8499958ea16496e84be30b07b2700633fd3636c
manulon <mlongoe@fi.uba.ar>
2022-10-05 11:02:40 -0300

    todo hermoso

--- TP1/fsm.vhdl
+++ TP1/fsm.vhdl
@@ -18,7 +18,7 @@ end fsm;
 
 architecture behavioral of fsm is
     
-    type t_state is (S0, S1, S2, S3, S4, S5, S6);
+    type t_state is (S0, S1, S2, S3, S4, S5);
     signal state : t_state;
     signal aux_inicial:  std_logic := '1';
 
@@ -51,21 +51,18 @@ begin
                     end if;
                 when S5 =>
                     if tres_segundos = '1' then
-                        state <= S6;
-                    end if;
-                when S6 =>
-                    if tres_segundos = '1' then
                         state <= S0;
                     end if;
+              
             end case;
         end if;
     end process;
 
     r1 <= '1' when (state = S2 or state = S3 or state = S4 ) else '0';
-    r2 <= '1' when (state = S0 or state = S1 or state = S5 or state = S6) else '0';
+    r2 <= '1' when (state = S0 or state = S1 or state = S5 ) else '0';
     a1 <= '1' when (state = S1 or state = S5) else '0';
     a2 <= '1' when (state = S2 or state = S4) else '0';
-    v1 <= '1' when (state = S0 or state = S6) else '0';
+    v1 <= '1' when (state = S0) else '0';
     v2 <= '1' when (state = S3) else '0';
 
 end behavioral;
